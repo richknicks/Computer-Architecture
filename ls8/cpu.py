f4c0523fd59ec1578fd27e711687365fc8d6bb1c
richknicks <rlynch@gmail.com>
2020-07-22 05:03:47 -0400

    Day 1 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,32 @@
 
 import sys
 
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+"""
+LDI: load "immediate", store a value in a register, or "set this register to this value".
+PRN: a pseudo-instruction that prints the numeric value stored in a register.
+HLT: halt the CPU and exit the emulator.
+"""
+
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+        self.running = True
+        # stack pointer needed
+
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +38,24 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -46,20 +65,41 @@ class CPU:
         from run() if you need help debugging.
         """
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
+        print(
+            f"TRACE: %02X | %02X %02X %02X |"
+            % (
+                self.pc,
+                # self.fl,
+                # self.ie,
+                self.ram_read(self.pc),
+                self.ram_read(self.pc + 1),
+                self.ram_read(self.pc + 2),
+            ),
+            end="",
+        )
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.reg[i], end="")
 
         print()
 
     def run(self):
         """Run the CPU."""
-        pass
+        running = True
+        while running:
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if IR == LDI:
+                self.reg[operand_a] = 1
+                self.pc += 3
+
+            if IR == PRN:
+                print(self.pc - 1)
+
+            if IR == HLT:
+                running = False
+
+            self.pc += 1
+
--- simple.py
+++ simple.py
@@ -1,43 +1,41 @@
-
-PRINT_TIM  = 0b01
-HALT       = 0b10 #2
-PRINT_NUM  = 0b11 # opcode 3
-SAVE       = 0b100
-PRINT_REG  = 0b101
-ADD        = 0b110
+PRINT_TIM = 0b01
+HALT = 0b10  # 2
+PRINT_NUM = 0b11  # opcode 3
+SAVE = 0b100
+PRINT_REG = 0b101
+ADD = 0b110
 # save the number 99 into R2
 # print whatever is inside R2
 memory = [
-    PRINT_TIM, # <-- PC
+    PRINT_TIM,  # <-- PC
     PRINT_TIM,
     PRINT_NUM,
     42,
     SAVE,
-    2, # register to save it in 
-    99, # number to save
-    SAVE, 
-    3, # register to save it in 
-    1, # number to save 
+    2,  # Save in register 2
+    99,  # number to save in register 2
+    SAVE,
+    3,  # Save in register 3
+    1,  # number to save in register 3
     ADD,
     2,
     3,
     PRINT_REG,
-    2, # register to look at 
-    HALT
+    2,  # register to look in
+    HALT,
 ]
 
 # write a program to pull each command out of memory and execute
 # We can loop over it!
 
-# register aka memory
-registers = [0] * 8
-# [0,0,99,0,0,0,0,0] 
+registers = [0] * 8  # 8 general purpose registers, like variables, R0, R1, R2, ...R7
+# [0,0,99,0,0,0,0,0]
 # R0 - R7
 
-pc = 0 # program counter, index of the current instruction
+pc = 0  # program counter, index of the current instruction, special purpose register
 running = True
 while running:
-    command = memeory[pc] # Instruction register
+    command = memory[pc]  # Instruction register
 
     if command == PRINT_TIM:
         print("Tim!")
@@ -58,10 +56,10 @@ while running:
         pc += 2
 
     if command == PRINT_REG:
-        reg = memory[pc + 1] 
-        print (registers[reg_index])
+        reg = memory[pc + 1]
+        print(registers[reg])
         pc += 1
 
     if command == ADD:
 
-    pc += 1
+        pc += 1
